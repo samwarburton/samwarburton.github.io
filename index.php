4c44607907656e9e29488027e8c72aae37c515fc
Jess <Sam@SAMUELs-MacBook-Pro.local>
2017-05-10 20:46:59 +1000

    new stuff

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>	
+
+	<link rel="stylesheet" href="css/style.css">
+
+
+	<title>Sam Warburton</title>
+</head>
+<body>
+
+
+
+	<div class="jumbotron animated">
+	<div class="container text-center">
+	<h1>Sam Warburton</h1>
+	<p><i>Web Developer From Melbourne</i></p>
+
+	</div>
+	
+	</div>
+
+
+	<div class="container">
+		<div class="col-md-8 col-md-offset-2">
+			<h3>Information</h3>
+			<p>
+			Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the indus
+			try's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five c
+			enturies, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop 
+			publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+			</p>
+
+			<h3>Portfolio</h3>
+			<p>
+			Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the indus
+			try's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five c
+			enturies, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop 
+			publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+			</p>
+			<div class="row">
+				<div class="col-md-4">
+					<img src="https://placehold.it/300.png/09f/fff" class="img img-responsive">
+				</div>
+				<div class="col-md-4">
+					<img src="https://placehold.it/300.png/09f/fff" class="img img-responsive">
+				</div>
+				<div class="col-md-4">
+					<img src="https://placehold.it/300.png/09f/fff" class="img img-responsive">
+				</div>
+
+
+			</div>
+
+
+		</div>
+
+	</div>
+
+
+
+
+
+
+
+</body>
+</html>
